f36227262526ed4868133b88f1cff0d2208f2371
aryaw05 <aryawahyuaw77@gmail.com>
2025-07-31 18:47:43 +0700

    feat : add motors get API

---
+++ app/api/motors/motors.ts
@@ -0,0 +1,19 @@
+import Cookies from "js-cookie";
+export async function getMotors() {
+  const token = Cookies.get("token");
+  const result = await fetch("http://localhost:3000/api/motor", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token || "",
+    },
+  });
+  if (!result.ok) {
+    console.error("Gagal mengambil data motor:", result.statusText);
+  }
+  const data = await result.json();
+
+  return data;
+}
+
+export async function getImage() {}
