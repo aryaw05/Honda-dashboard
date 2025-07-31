825f2515abdc82eb21ff8bcae57a7638e3626ec7
aryaw05 <aryawahyuaw77@gmail.com>
2025-07-31 19:19:07 +0700

    feat  : add iamges data

--- components/fragments/card.tsx
+++ components/fragments/card.tsx
@@ -9,16 +9,15 @@ import {
 } from "@/components/ui/card";
 import Image from "next/image";
 
-export default function MotorCard() {
+export default function MotorCard(props: any) {
+  const { image } = props;
   return (
-    <Card className="w-[290px] mx-auto ">
+    <Card className="w-[290px] mx-auto cursor-pointer border">
       <CardHeader>
         <Image
           className="max-h-30 object-cover rounded-lg"
-          src={
-            "https://images.unsplash.com/photo-1529226014005-9db6241b4768?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          }
-          alt="Image"
+          src={`http://localhost:3000/uploads/${image}`}
+          alt="Images"
           width={500}
           height={500}
         />
