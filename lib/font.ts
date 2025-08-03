f880c99654378b1ba077a0497eb363b33d918484
aryaw05 <aryawahyuaw77@gmail.com>
2025-08-03 19:34:07 +0700

    feat(fonts): add Space_Grotesk font configuration

--- lib/font.ts
+++ lib/font.ts
@@ -1,9 +1,13 @@
-import { Geist, Inter } from "next/font/google";
+import { Geist, Inter, Space_Grotesk } from "next/font/google";
 
 export const geist = Geist({
   subsets: ["latin"],
 });
 
 export const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const grotesk = Space_Grotesk({
   subsets: ["latin"],
 });
