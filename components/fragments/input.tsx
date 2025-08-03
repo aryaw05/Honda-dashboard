a0487ec47d8546f62add48eff7a4c11fe9062128
aryaw05 <aryawahyuaw77@gmail.com>
2025-08-03 19:36:04 +0700

    feat(form-components): enhance input and select components with form handling
    - Add onChange and name props to InputData component for form integration
    - Update Select component to handle form submission with proper value mapping
    - Improve styling consistency with gap and font adjustments

--- components/fragments/input.tsx
+++ components/fragments/input.tsx
@@ -1,17 +1,19 @@
 import { Input } from "../ui/input";
 
 export default function InputData(props: any) {
-  const { placeholder, key, type, label, multiple } = props;
+  const { placeholder, key, type, label, multiple, onChange, name } = props;
   return (
-    <div>
-      <label className="text-lg">{label}</label>
+    <div className="flex flex-col gap-3">
+      <label className="text-xl font-semibold">{label}</label>
       <Input
+        onChange={onChange}
         multiple={multiple ? multiple : false}
         required
         key={key}
         placeholder={placeholder}
         className="w-full py-10 px-5 rounded-xl md:text-lg "
         type={type ? type : "text"}
+        name={name}
       />
     </div>
   );
--- components/fragments/select.tsx
+++ components/fragments/select.tsx
@@ -9,30 +9,35 @@ import {
 } from "@/components/ui/select";
 
 export default function InputSelect(props: any) {
-  const { data } = props;
-  console.log(data);
+  const { data, onChange, name } = props;
 
   return (
-    <div>
+    <div className="flex flex-col gap-3">
       <label className="text-lg">Category</label>
-      <Select>
+      <Select onValueChange={(value) => onChange({ target: { name, value } })}>
         <SelectTrigger className="w-full rounded-xl p-10 text-lg">
           <SelectValue placeholder="Select Category" />
         </SelectTrigger>
         <SelectContent>
           <SelectGroup>
             <SelectLabel className="text-md text-muted-foreground">
-              Fruits
+              Category
             </SelectLabel>
-            {data.map((item: any, index: number) => (
-              <SelectItem
-                key={index}
-                value={item.nama_kategori}
-                className="text-lg"
-              >
-                {item.nama_kategori}
-              </SelectItem>
-            ))}
+            {data.map(
+              (
+                item: { id_kategori: number; nama_kategori: string },
+                index: number
+              ) => (
+                <SelectItem
+                  onChange={onChange}
+                  key={index}
+                  value={item.id_kategori}
+                  className="text-lg"
+                >
+                  {item.nama_kategori}
+                </SelectItem>
+              )
+            )}
           </SelectGroup>
         </SelectContent>
       </Select>
