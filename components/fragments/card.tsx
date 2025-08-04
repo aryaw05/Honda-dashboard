d1c8a40fb23b997e266daac6cb7ea27d38f262fe
aryaw05 <aryawahyuaw77@gmail.com>
2025-08-04 19:52:42 +0700

    feat(card): refactor MotorCard component to use typed props and improve layout

--- components/fragments/card.tsx
+++ components/fragments/card.tsx
@@ -4,28 +4,28 @@ import {
   CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
+import { Motor } from "@/lib/types/motor";
 import Image from "next/image";
 
-export default function MotorCard(props: any) {
-  const { image, deskripsi, nama_barang, harga } = props;
+export default function MotorCard(props: Motor) {
+  const { gambar_card, deskripsi, nama_barang, harga } = props;
   return (
-    <Card className="w-[290px] mx-auto cursor-pointer border rounded-xl">
-      <CardHeader className="px-0 pt-0">
+    <Card className="w-[290px] mx-auto cursor-pointer  border rounded-xl">
+      <CardHeader className="px-3">
         <Image
           className="max-h-48 object-cover rounded-lg"
-          src={`http://localhost:3000/uploads/${image}`}
+          src={`http://localhost:3000/uploads/${gambar_card}`}
           alt="Images"
           width={500}
           height={500}
         />
       </CardHeader>
-      <CardContent className="mt-0 ">
+      <CardContent className="mt-0 text-xl">
         <p>{nama_barang} </p>
         <CardDescription>{deskripsi}</CardDescription>
       </CardContent>
-      <CardFooter>
+      <CardFooter className="text-2xl">
         <p>Rp.{harga}</p>
       </CardFooter>
     </Card>
