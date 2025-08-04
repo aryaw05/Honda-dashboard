e7d6c42e3c891d023cebd67e4ed8e3dd99b67432
aryaw05 <aryawahyuaw77@gmail.com>
2025-08-04 19:53:02 +0700

    feat(textarea): add onChange prop to TextArea component for better form handling

--- components/fragments/textArea.tsx
+++ components/fragments/textArea.tsx
@@ -1,12 +1,14 @@
 import { Textarea } from "../ui/textarea";
 
 export default function TextArea(props: any) {
-  const { placeholder, label, className } = props;
+  const { placeholder, label, className, onChange } = props;
+
   return (
     <div className={className}>
       <label className="text-lg">{label}</label>
       <Textarea
         placeholder={placeholder}
+        onChange={onChange}
         className=" h-75 md:text-lg rounded-xl"
       />
     </div>
